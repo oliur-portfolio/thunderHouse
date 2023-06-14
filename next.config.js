fdafcb55e7aa00bc087473ced1856286fa57e500
Oliur Rahman <oliurr941@gmail.com>
2023-06-14 15:48:13 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "no-store, must-revalidate",
-                    },
-                ],
-            },
-            {
-                source: "/_next/(.*)",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "public, max-age=0, s-maxage=604800, stale-while-revalidate=86400",
-                    },
-                ],
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
