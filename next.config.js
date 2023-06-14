b444ba9fec0fb2ef8c2c72b0adc80b1445641a1f
Oliur Rahman <oliurr941@gmail.com>
2023-06-14 17:31:33 +0600

    fix redirect issue

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     async redirects() {
         return [
             {
+                source: "/insights",
+                destination: "/",
+                permanent: true,
+            },
+            {
+                source: "/competencies",
+                destination: "/",
+                permanent: true,
+            },
+            {
                 source: "/contact",
                 destination: "/",
                 permanent: true,
